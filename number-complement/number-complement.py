97283bd5ecf4dc5a6d22c638a4564a9b13d28191
aaronlee232 <aaronlee232@gmail.com>
2021-12-28 02:13:59 -0600

    Time: 32 ms (55.65%), Space: 14.2 MB (68.43%) - LeetHub

--- number-complement/number-complement.py
+++ number-complement/number-complement.py
@@ -1,5 +1,6 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        bits = int(log(num, 2)) + 1
-        mask = 2 ** bits - 1
-        return num ^ mask
+        # mask for 3,4,5,6 bits = 111, 1111, 11111, 111111, so on
+        # creates a mask by shifting over 1 by n bits to create n number of trailing 0s with 1 extra bit at the front
+        # subtracting 1 then reduces mask to correct length and fills in with all 1s
+        return num ^ ((1 << num.bit_length()) - 1)
