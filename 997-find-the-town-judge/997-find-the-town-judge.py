b66dfac85fc354335e27799eb86c35b5887940a3
aaronlee232 <aaronlee232@gmail.com>
2022-01-03 17:04:58 -0600

    Time: 1654 ms (5.02%), Space: 18.9 MB (87.27%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -1,15 +1,14 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         # non-zero based indexing
-        trustsNobody = [True] * (n + 1)
-        trustCount = [0] * (n + 1)
+        trustScore = [0] * (n + 1)
         
         for person in trust:
-            trustsNobody[person[0]] = False
-            trustCount[person[1]] += 1
+            trustScore[person[0]] -= 1
+            trustScore[person[1]] += 1
         
         for person in range(1, n + 1):
-            if trustsNobody[person] and trustCount[person] == n - 1:
+            if trustScore[person] == n - 1:
                 return person
         return -1
         
