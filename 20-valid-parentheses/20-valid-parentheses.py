03b1ce2950b29ba4878ca12340dfae42427c8b58
aaronlee232 <aaronlee232@gmail.com>
2022-01-28 16:55:18 -0600

    Time: 28 ms (90.94%), Space: 14 MB (99.35%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,44 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        #in order ( { [
-        brackets = [0, 0, 0]
-        expected = []
+        #stack to track most recent parenthesis
+        #only add opening brackets to stack
+        seen = []
+        brackets = {'(':')', '{':'}', '[':']'}
         
         for c in s:
-            if c == '(':
-                brackets[0] += 1
-                expected.append(')')
-            elif c == '{':
-                brackets[1] += 1
-                expected.append('}')
-            elif c == '[':
-                brackets[2] += 1
-                expected.append(']')
-            elif len(expected) > 0:
-                if c == ')':
-                    if expected.pop() == c:
-                        brackets[0] -= 1
-                        if brackets[0] < 0:
-                            return False
-                    else:
-                        return False
-                elif c == '}':
-                    if expected.pop() == c:
-                        brackets[1] -= 1
-                        if brackets[1] < 0:
-                            return False
-                    else:
-                        return False
-                elif c == ']':
-                    if expected.pop() == c:
-                        brackets[2] -= 1
-                        if brackets[2] < 0:
-                            return False
-                    else:
-                        return False
+            if c in brackets.keys():
+                seen.append(brackets[c])
             else:
-                return False
-        if sum(brackets) == 0:
-            return True
-        else:
-            return False
+                if not seen or seen.pop() != c:
+                    return False
+        return len(seen) == 0
